9d01ffc3c7e60958f68d184d66f4e547cf96d0c5
Wesley Gomes Santos <motoca@gmail.com>
2021-12-21 23:48:46 -0300

    versão com firebase

---
+++ src/pages/teste/index.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, doc, setDoc } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, 
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default function Home() {
+    
+    getClientes().then((clientes) => {
+        for (const cliente of clientes) {
+            console.log("Nome: "+cliente.nome)
+        }
+    })
+
+    return "CLIENTES: ";
+}
+
+async function getClientes() {
+    const clientesCol = collection(db, 'clientes');
+    const clientesSnapshot = await getDocs(clientesCol);
+    const clientesList = clientesSnapshot.docs.map(doc => doc.data());
+    return clientesList;
+}
